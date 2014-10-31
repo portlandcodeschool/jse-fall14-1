6c893245e5a5aaf1d10a1dbd83390d6a32e81e09
Stacy Fabian <stacy.fabian@gmail.com>
2014-10-31 03:49:37 -0700

    Found typos and fixed.

--- stacyFabian/stacyFabian_week1_question4.js
+++ stacyFabian/stacyFabian_week1_question4.js
@@ -1,16 +1,14 @@
 // Week 1 Question 4
 // Improper Fractions
 
-var n, d, remainder, fullNums; // 7, 4, remain
+var n, d;
 
 var impropFrac = n +"/" + d; // 7/4
 
 var remainder = (n%d);  // 7%4 = 3
 
-var fullNums = n - remainder;
+var evenlyDivisible = n - remainder;
 
-var remain = (n%d);  // 7%4 = 3
-
-wholeNums = fullNums /  remainder;
+var wholeNums = evenlyDivisible /  remainder;
 
 var propFrac = fullNums + " " + remainder + "/" + d;
