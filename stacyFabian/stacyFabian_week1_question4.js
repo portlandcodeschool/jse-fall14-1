ee95f7bf886c8367aaef6abd6aa482773ba3dd97
Stacy Fabian <stacy.fabian@gmail.com>
2014-10-31 03:43:29 -0700

    Updated file name from stacyFabian_week1_question1.js to question4.js

---
+++ stacyFabian/stacyFabian_week1_question4.js
@@ -0,0 +1,16 @@
+// Week 1 Question 4
+// Improper Fractions
+
+var n, d, remainder, fullNums; // 7, 4, remain
+
+var impropFrac = n +"/" + d; // 7/4
+
+var remainder = (n%d);  // 7%4 = 3
+
+var fullNums = n - remainder;
+
+var remain = (n%d);  // 7%4 = 3
+
+wholeNums = fullNums /  remainder;
+
+var propFrac = fullNums + " " + remainder + "/" + d;
